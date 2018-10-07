eddfeab3b2a846998824b2e297db4b30a1f395bf
Lucien Najev <luciennajev@hotmail.com>
2018-10-07 19:11:11 +0100

    added a card class with tests

--- lib/card.rb
+++ lib/card.rb
@@ -1,17 +1,31 @@
 class Card  
+  
+  attr_reader :rank, :suit
 
-    attr_reader :rank, :suit
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
 
-    def initialize(rank, suit)
-        @rank = rank
-        @suit = suit
-    end
+  def description
+    return "#{rank} of #{suit}"
+  end
 
-
-    def description
-        return "#{rank} of #{suit}"
-    end
-
-
+  def value
+    scores = {
+      "Two" => 2,
+      "Three" => 3,
+      "Four" => 4,
+      "Five" => 5,
+      "Six" => 6,
+      "Seven" => 7,
+      "Eight" => 8,
+      "Nine" => 9,
+      "Jack" => 10,
+      "Queen" => 10,
+      "King" => 10,
+      "Ace" => 11
+    }
+    return scores[rank]
+  end
 end
-
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,12 +1,16 @@
 require 'card'
 
 describe Card do  
-    subject(:card) { described_class.new("Ace", "Spades") }
+  subject(:card) { described_class.new("Ace", "Spades") }
 
-    describe '#description' do
-        it "returns a string that describes itself" do
-            expect(card.description).to eq("Ace of Spades")
-        end
+  describe '#description' do
+    it "returns a string that describes itself" do
+      expect(card.description).to eq("Ace of Spades")
     end
+  end
+  describe '#value' do
+    it "is able to calculate its value" do
+      expect(card.value).to eq(11)
+    end
+  end
 end
-
