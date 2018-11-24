ad9e4d6f1edfad0482b6882430dd380eb61d1b93
Lucien Najev <luciennajev@hotmail.com>
2018-11-24 13:41:48 +0000

    refactor

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,9 +6,15 @@ describe Game do
   subject(:game) { described_class.new }
 
   describe '#initialize' do
+
     it 'initializes with 2 players with no cards - empty arrays' do
       expect(game.sam).to eq([])
       expect(game.dealer).to eq([])
     end
+
+    it 'initializes with a deck of 52 cards' do
+      expect(game.deck.cards.size).to eq(52)
+    end
+
   end
 end
