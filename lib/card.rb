2ca8bf94ad79c0a7739241bb71abed90cbdca0a4
Lucien Najev <luciennajev@hotmail.com>
2018-11-24 13:16:05 +0000

    refactoring: seperated out the scores into a module with constant variable hash

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,8 @@
-class Card  
-  
+require './lib/scores'
+
+class Card
+  include Scores
+
   attr_reader :rank, :suit
 
   def initialize(rank, suit)
@@ -8,24 +11,10 @@ class Card
   end
 
   def description
-    return "#{rank} of #{suit}"
+    "#{rank} of #{suit}"
   end
 
   def value
-    scores = {
-      "Two" => 2,
-      "Three" => 3,
-      "Four" => 4,
-      "Five" => 5,
-      "Six" => 6,
-      "Seven" => 7,
-      "Eight" => 8,
-      "Nine" => 9,
-      "Jack" => 10,
-      "Queen" => 10,
-      "King" => 10,
-      "Ace" => 11
-    }
-    return scores[rank]
+    Scores::SCORES[rank]
   end
 end
